016a4c9be59d3b7b7c07a02eaf60d21ad5b91cb1
ababup1192 <ababup1192@gmail.com>
2016-10-18 17:28:02 +0900

    Update React example

--- src/script/components/App.tsx
+++ src/script/components/App.tsx
@@ -1,27 +1,64 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { List } from "immutable";
 
 interface IAppProps {
-    message: string;
+    upperList: List<number>;
+    lowerList: List<number>;
 }
 
 interface IAppState {
-    clicked: boolean;
+    upperList: List<number>;
+    lowerList: List<number>;
 }
 
 export default class App extends React.Component<IAppProps, IAppState> {
     constructor(props) {
         super(props);
-        this.state = { clicked: false };
+        this.state = props;
     }
 
-    handledClick(e) {
-        this.setState({ clicked: !this.state.clicked });
+    handledClickUpper(e, key: number) {
+        // 状態(List)の変更
+        const upperList = this.state.upperList.filterNot((l) => l == key).toList();
+        const lowerList = this.state.lowerList.push(key);
+
+        this.setState({ upperList: upperList, lowerList: lowerList });
+    }
+
+    handledClickLower(e, key: number) {
+        // 状態(List)の変更
+        const upperList = this.state.upperList.push(key);
+        const lowerList = this.state.lowerList.filterNot((l) => l == key).toList();
+
+        this.setState({ upperList: upperList, lowerList: lowerList });
     }
 
     render() {
-        const message = this.state.clicked ? this.props.message : "Click ME!";
+        // Listの生成
+        const upperList = <ul className="upperList">
+            {this.state.upperList.map((l) =>
+                <li
+                    key={l}
+                    onClick={(e) => this.handledClickUpper(e, l)}>
+                    {l}
+                </li>)
+            }
+        </ul>;
 
-        return <h1 onClick={(e) => this.handledClick(e)}>{message}</h1>;
+        const lowerList = <ul className="lowerList">
+            {this.state.lowerList.map((l) =>
+                <li
+                    key={l}
+                    onClick={(e) => this.handledClickLower(e, l)}>
+                    {l}
+                </li>)
+            }
+        </ul>;
+
+        return <div className="listContainer">
+            {upperList}
+            {lowerList}
+        </div>;
     }
 }
--- src/script/index.tsx
+++ src/script/index.tsx
@@ -3,10 +3,15 @@ import "../style/main.css";
 
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { List } from "immutable";
 
 // import main typescript
 import App from "./components/App";
 
-const appNode = document.getElementById("content");
+// keyが属性, valueが値としてpropsになる。
+const lists = {
+    upperList: List.of(0, 1, 2, 3, 4),
+    lowerList: List.of(5, 6, 7, 8, 9)
+};
 
-ReactDOM.render(<App message="Hyahhhaaaaaaa!!" />, appNode);
+ReactDOM.render(<App {...lists} />, document.getElementById("content"));
